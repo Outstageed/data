0888cc833b0876647b8ab0543e210d05dd9a9d16
Razi Falah <92949627+RaziFalah@users.noreply.github.com>
2022-03-10 20:57:28 +0200

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,4 +1,13 @@
-If true:
-  do true
+from time import sleep
+
+
+While True:
+  for 1 != 0:
+    id1 = 13589
+    Execution(1, id1)
 else:
-  do false
+  for 1 != 0:
+    id0 = 96289
+    Execution(0, id0)
+    sleep(20)
+    exit()
